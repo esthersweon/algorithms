4a2ad7a158082d106b084c87d9557044e075f108
Esther Shuhie Weon <esthershuhieweon@Esthers-MacBook-Pro-2.local>
2018-07-16 22:58:01 -0700

    Linked List problems – first half; create files for stacks and queues

--- linked_lists/implementation.py
+++ linked_lists/implementation.py
@@ -25,47 +25,68 @@ class LinkedList:
         self.head = head
 
     def append_to_tail(self, val):
-        currentNode = self.head
+        current_node = self.head
 
-        while currentNode.next is not None:
-            currentNode = currentNode.next
+        while current_node.next is not None:
+            current_node = current_node.next
         
-        currentNode.append(val)
+        current_node.append(val)
+    
+    def get_vals(self):
+        vals = []
+        current_node = self.head
+
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        
+        return vals
+
+    def get_length(self):
+        counter = 0
+        current_node = self.head
+
+        while current_node is not None:
+            counter += 1
+            current_node = current_node.next
+        return counter
 
 first_node = SinglyLinkedListNode(1)
 my_sll = LinkedList(first_node)
 my_sll.append_to_tail(2)
 
-print('Singly Linked List:', my_sll)
-print('Head:', my_sll.head)
-print('Head Value:', my_sll.head.val)
-print('Next Node:', my_sll.head.next)
-print('Next Node Value:', my_sll.head.next.val)
-print('Next Node:', my_sll.head.next.next)
+# print('Singly Linked List:', my_sll)
+# print('Head:', my_sll.head)
+# print('Head Value:', my_sll.head.val)
+# print('Next Node:', my_sll.head.next)
+# print('Next Node Value:', my_sll.head.next.val)
+# print('Next Node:', my_sll.head.next.next)
+# print('Total Length:', my_sll.get_length())
 
 third_node = DoublyLinkedListNode(3)
 my_dll = LinkedList(third_node)
 my_dll.append_to_tail(4)
 my_dll.append_to_tail(5)
 
-print('\nDoubly Linked List:', my_dll)
-print('Head:', my_dll.head)
-print('Head Value:', my_dll.head.val)
-print('Next Node:', my_dll.head.next)
-print('Next Node Value:', my_dll.head.next.val)
-print("Next Node's Previous (Head Node):", my_dll.head.next.prev)
-print("Next Node's Previous Value (Head Value):", my_dll.head.next.prev.val)
-print('Next Node:', my_dll.head.next.next)
-print('Next Node Value:', my_dll.head.next.next.val)
+# print('\nDoubly Linked List:', my_dll)
+# print('Head:', my_dll.head)
+# print('Head Value:', my_dll.head.val)
+# print('Next Node:', my_dll.head.next)
+# print('Next Node Value:', my_dll.head.next.val)
+# print("Next Node's Previous (Head Node):", my_dll.head.next.prev)
+# print("Next Node's Previous Value (Head Value):", my_dll.head.next.prev.val)
+# print('Next Node:', my_dll.head.next.next)
+# print('Next Node Value:', my_dll.head.next.next.val)
+# print('Total Length:', my_dll.get_length())
 
-print('\n Deleted 2nd Node')
+# print('\nDeleted 2nd Node')
 my_dll.head.next.delete()
 
-print('\nNew Doubly Linked List:', my_dll)
-print('Head:', my_dll.head)
-print('Head Value:', my_dll.head.val)
-print('Next Node:', my_dll.head.next)
-print('Next Node Value:', my_dll.head.next.val)
-print("Next Node's Previous (Head Node):", my_dll.head.next.prev)
-print("Next Node's Previous Value (Head Value):", my_dll.head.next.prev.val)
-print('Next Node:', my_dll.head.next.next)
+# print('\nNew Doubly Linked List:', my_dll)
+# print('Head:', my_dll.head)
+# print('Head Value:', my_dll.head.val)
+# print('Next Node:', my_dll.head.next)
+# print('Next Node Value:', my_dll.head.next.val)
+# print("Next Node's Previous (Head Node):", my_dll.head.next.prev)
+# print("Next Node's Previous Value (Head Value):", my_dll.head.next.prev.val)
+# print('Next Node:', my_dll.head.next.next)
--- linked_lists/problems.py
+++ linked_lists/problems.py
@@ -1,38 +1,106 @@
+from implementation import SinglyLinkedListNode, LinkedList
+
+# Helper function
+def create_singly_linked_list(vals):
+    my_node = SinglyLinkedListNode(vals[0])
+    my_sll = LinkedList(my_node)
+
+    for val in vals[1:]:
+        my_sll.append_to_tail(val)
+
+    return my_sll
+
+my_sll = create_singly_linked_list([1, 2, 2, 2, 4])
+
+# Problems
 def remove_dupes(ll):
     values = []
-    current_node = ll['head']
-    placeholder_node = ll['head']
+    current_node = ll.head
+    placeholder_node = ll.head
     is_skipping = False
 
     while current_node is not None:
-        if current_node['val'] not in values:
+        if current_node.val not in values:
             if is_skipping:
-                placeholder_node['next'] = current_node
+                placeholder_node.next = current_node
                 placeholder_node = current_node
             else:
-                values.append(current_node['val'])
+                values.append(current_node.val)
                 placeholder_node = current_node
         else:
             if not is_skipping:
                 is_skipping = True
         
-        current_node = current_node['next']
+        current_node = current_node.next
     
     if is_skipping:
-        placeholder_node['next'] = None
+        placeholder_node.next = None
     
     return ll
 
 print('** remove_dupes **')
-print(remove_dupes({'head': {'val': 1, 'next': {'val': 2, 'next': {'val': 3, 'next': {'val': 1, 'next': {'val': 3, 'next': {'val': 5, 'next': None}}}}}}}))
-print(remove_dupes({'head': {'val': 1, 'next': {'val': 2, 'next': {'val': 3, 'next': {'val': 1, 'next': {'val': 3, 'next': {'val': 2, 'next': None}}}}}}}))
+print('Started with', my_sll.get_length(), 'nodes')
+print('Removed duplicates:', remove_dupes(my_sll))
+print('Ended with', my_sll.get_length(), 'nodes')
 
-def remove_dupes_without_buffer(ll):
-    pointer1 = ll['head']
-    pointer2 = pointer1['next']
+# def remove_dupes_without_buffer(ll):
+#     pointer1 = ll.head
+#     pointer2 = pointer1.next
 
+# print('** remove_dupes_without_buffer **')
+# print(remove_dupes_without_buffer(my_dll)
+
+def get_kth_to_last_node(k, sll):
+    ll_length = sll.get_length()
+    kth_to_last_node = ll_length - k
+
+    if kth_to_last_node <= 0:
+        return 'Linked list is not long enough to get {} from last node'.format(k)
+    else:
+        counter = 1
+        current_node = sll.head
+
+        while counter is not kth_to_last_node:
+            current_node = current_node.next
+            counter += 1
+        return current_node.val
+
+my_sll = create_singly_linked_list([0, 1, 2, 3])
+
+print('** get_kth_to_last_node **')
+print('get_kth_to_last_node(0, my_sll):', get_kth_to_last_node(0, my_sll))
+print('get_kth_to_last_node(2, my_sll):', get_kth_to_last_node(2, my_sll))
+print('get_kth_to_last_node(3, my_sll):', get_kth_to_last_node(3, my_sll))
+print('get_kth_to_last_node(4, my_sll):', get_kth_to_last_node(4, my_sll))
+
+my_sll = create_singly_linked_list([0, 1, 2, 3])
+
+def delete_middle_node(val, sll):
+    placeholder_node = sll.head
+    current_node = sll.head.next
+
+    while current_node is not None:
+        if current_node.val == val:
+            placeholder_node.next = current_node.next
+        placeholder_node = current_node
+        current_node = current_node.next
     
+    return sll
 
-print('** remove_dupes_without_buffer **')
-print(remove_dupes_without_buffer({'head': {'val': 1, 'next': {'val': 2, 'next': {'val': 3, 'next': {'val': 1, 'next': {'val': 3, 'next': {'val': 5, 'next': None}}}}}}}))
-print(remove_dupes_without_buffer({'head': {'val': 1, 'next': {'val': 2, 'next': {'val': 3, 'next': {'val': 1, 'next': {'val': 3, 'next': {'val': 2, 'next': None}}}}}}}))
+print('** delete_middle_node **')
+print('delete_middle_node(1, my_sll):', delete_middle_node(1, my_sll).get_vals())
+print('delete_middle_node(2, my_sll):', delete_middle_node(2, my_sll).get_vals())
+
+my_sll = create_singly_linked_list([3, 5, 8, 5, 10, 2, 1])
+
+def partition(sll, partition):
+    nodes_over = []
+    current_node = sll.head
+    
+    while current_node is not None:
+        if current_node.val >= partition:
+            
+        current_node = current_node.next
+
+print('** partition **')
+print('partition(1, my_sll):', partition(my_sll, 5).get_vals())
